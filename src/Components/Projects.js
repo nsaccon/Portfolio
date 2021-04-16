f4af2effd0117edb8b3049194fab45bf466c7826
Nathan <nathansaccon10@hotmail.com>
2021-04-15 21:03:02 -0400

    Updated reveals for projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,8 @@
 import { Grid } from "@material-ui/core";
 import Fade from "react-reveal/Fade";
-import Flip from "react-reveal/Flip";
 import { GoBeaker } from "react-icons/go";
-import imgCollegeM8 from '../Images/CollegeM8.png'
-import imgDocuSign from '../Images/DocuSign.png'
+import imgCollegeM8 from "../Images/CollegeM8.png";
+import imgDocuSign from "../Images/DocuSign.png";
 
 const Projects = () => {
   return (
@@ -12,44 +11,72 @@ const Projects = () => {
         <Grid item xs={12}>
           <Fade top delay={100}>
             <h3 className="title">
-              <GoBeaker size="0.8em" className='popIcon' /> Projects
+              <GoBeaker size="0.8em" className="popIcon" /> Projects
             </h3>
           </Fade>
         </Grid>
 
         <Grid item xs={4}>
-        <img src={imgCollegeM8}
-                title="CollegeM8 Login Screen" 
-                alt="Screenshot of CollegeM8 Login Screen" 
-                className="project-image" />
+          <Fade left delay={600}>
+          <img
+            src={imgCollegeM8}
+            title="CollegeM8 Login Screen"
+            alt="Screenshot of CollegeM8 Login Screen"
+            className="project-image"
+          />
+            </Fade>
+          
         </Grid>
         <Grid item xs={6}>
-            <div align='left'>
-
+          <div className='project-container'>
+          <Fade right delay={600}>
           <h4 className="project-title">CollegeM8</h4>
-          <p className="project-text">
-                Developed a professional 3D game in the span of 3 months, using C# MonoGame for the course <b>Game Programming with Data Structures</b>. Designed with custom 2D collision detections, billboard particle emissions, frustum model culling, and for efficient garbage and resource management.
-              </p>
+            </Fade>
+            <Fade right delay={200}>
+            <p className="project-text">
+              Developed a professional 3D game in the span of 3 months, using C#
+              MonoGame for the course{" "}
+              <b>Game Programming with Data Structures</b>. Designed with custom
+              2D collision detections, billboard particle emissions, frustum
+              model culling, and for efficient garbage and resource management.
+            </p>
+            </Fade>
+            <Fade right delay={500}>
+              <div>
+                <a href="https://github.com/nsaccon/CollegeM8Capstone">
+                  View on Github
+                </a>
               </div>
+            </Fade>
+          </div>
         </Grid>
 
-        
         <Grid item xs={6}>
-            <div align='left'>
-
+          <div align="left">
+          <Fade left delay={600}>
           <h4 className="project-title">DocuSign Integration</h4>
-          <p className="project-text">
-                Developed a professional 3D game in the span of 3 months, using C# MonoGame for the course <b>Game Programming with Data Structures</b>. Designed with custom 2D collision detections, billboard particle emissions, frustum model culling, and for efficient garbage and resource management.
-              </p>
-              </div>
+            </Fade>
+            <Fade left delay={200}>
+            <p className="project-text">
+              Developed a professional 3D game in the span of 3 months, using C#
+              MonoGame for the course{" "}
+              <b>Game Programming with Data Structures</b>. Designed with custom
+              2D collision detections, billboard particle emissions, frustum
+              model culling, and for efficient garbage and resource management.
+            </p>
+            </Fade>
+          </div>
         </Grid>
         <Grid item xs={4}>
-        <img src={imgDocuSign}
-                title="DocuSign Logo" 
-                alt="DocuSign Logo" 
-                className="project-image" />
+        <Fade right delay={600}>
+        <img
+            src={imgDocuSign}
+            title="DocuSign Logo"
+            alt="DocuSign Logo"
+            className="project-image"
+          />
+            </Fade>
         </Grid>
-
       </Grid>
     </div>
   );
