acca1f83b4e2c0b0625dd5d99a265d675beda627
Nathan <nathansaccon10@hotmail.com>
2021-04-15 20:43:27 -0400

    Style Updates

--- src/Components/SocialIcons.js
+++ src/Components/SocialIcons.js
@@ -1,20 +1,38 @@
-import { Grid } from '@material-ui/core';
-import Flip from "react-reveal/Flip";
+import { Grid } from "@material-ui/core";
+import Fade from "react-reveal/Fade";
 import { SocialIcon } from "react-social-icons";
 
 const SocialIcons = () => {
   return (
     <div>
       <Grid container lg={4}>
-        <Flip bottom delay={600}>
-          <SocialIcon url="https://github.com/nsaccon" fgColor="#ab5a35" bgColor='#d6b39a' />
-        </Flip>
-        <Flip bottom delay={800}>
-          <SocialIcon url="https://www.linkedin.com/in/nathansaccon/" fgColor="#ab5a35" bgColor='#d6b39a' />
-        </Flip>
-        <Flip bottom delay={1000}>
-          <SocialIcon url="mailto:nathansaccon10@hotmail.com" fgColor="#ab5a35" bgColor='#d6b39a' />
-        </Flip>
+        <Grid item xs={4}>
+          <Fade top delay={600}>
+            <SocialIcon
+              url="https://github.com/nsaccon"
+              fgColor="#ab5a35"
+              bgColor="#d6b39a"
+            />
+          </Fade>
+        </Grid>
+        <Grid item xs={4}>
+          <Fade top delay={900}>
+            <SocialIcon
+              url="https://www.linkedin.com/in/nathansaccon/"
+              fgColor="#ab5a35"
+              bgColor="#d6b39a"
+            />
+          </Fade>
+        </Grid>
+        <Grid item xs={4}>
+          <Fade top delay={1200}>
+            <SocialIcon
+              url="mailto:nathansaccon10@hotmail.com"
+              fgColor="#ab5a35"
+              bgColor="#d6b39a"
+            />
+          </Fade>
+        </Grid>
       </Grid>
     </div>
   );
