7a81fb6e7391a5aa01bbeabb9340e6352bbea9f9
Nathan <nathansaccon10@hotmail.com>
2021-04-18 10:32:57 -0400

    Added project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,6 +3,7 @@ import Fade from "react-reveal/Fade";
 import { GoBeaker } from "react-icons/go";
 import imgCollegeM8 from "../Images/CollegeM8.png";
 import imgDocuSign from "../Images/DocuSign.png";
+import imgDHL from "../Images/DHL.jpg";
 
 const Projects = () => {
   return (
@@ -33,8 +34,8 @@ const Projects = () => {
             </Fade>
             <Fade right delay={200}>
               <p className="project-text">
-                This project is a web-based <b>AI planning application</b> that is
-                designed to help students organize their schedules. Developed
+                This project is a web-based <b>AI planning application</b> that
+                is designed to help students organize their schedules. Developed
                 the API that interacts with the front-end, as well as the AI
                 algorithm that intelligently generates a schedule tailored to
                 the student. Implemented with C# and EF Core, and hosted on MS
@@ -52,22 +53,39 @@ const Projects = () => {
         </Grid>
 
         <Grid item xs={6}>
-          <div align="left">
+          <div className="project-container" align="left">
             <Fade left delay={600}>
-              <h4 className="project-title">DocuSign Integration</h4>
+              <h4 className="project-title">DailyHockeyLineups</h4>
             </Fade>
             <Fade left delay={200}>
               <p className="project-text">
-                This project is a C# console application that interacts with
-                Docusign’s API as part of a <b>billing system integration</b>. Created for a local law firm, this application
-                automatically charges clients for signed paperwork after the
-                signature process is completed. The need for this project arose
-                as part of the Covid-19 strategy to allow lawyers to work more
-                efficiently from home.
+                Daily fantasy hockey prediction application built with python.
+                Developed to <b>predict the outcome of NHL games</b>, and predict the
+                players that will be most valuable based on how much they cost
+                on DraftKings and FanDuel. At one time the predictions were
+                posted on a blog that accumulated more than 100,000 pageviews.
               </p>
+              <Fade right delay={500}>
+              <div>
+                <a href="https://github.com/nsaccon/DHLineups">
+                  View on Github
+                </a>
+              </div>
+            </Fade>
             </Fade>
           </div>
         </Grid>
+        <Grid item xs={4}>
+          <Fade right delay={600}>
+            <img
+              src={imgDHL}
+              title="Daily Hockey Lineups Logo"
+              alt="NHL Logo"
+              className="project-image"
+            />
+          </Fade>
+        </Grid>
+
         <Grid item xs={4}>
           <Fade right delay={600}>
             <img
@@ -77,6 +95,24 @@ const Projects = () => {
               className="project-image"
             />
           </Fade>
+        </Grid>
+        <Grid item xs={6}>
+          <div className="project-container" align="left">
+            <Fade left delay={600}>
+              <h4 className="project-title">DocuSign Integration</h4>
+            </Fade>
+            <Fade left delay={200}>
+              <p className="project-text">
+                This project is a C# console application that interacts with
+                Docusign’s API as part of a <b>billing system integration</b>.
+                Created for a local law firm, this application automatically
+                charges clients for signed paperwork after the signature process
+                is completed. The need for this project arose as part of the
+                Covid-19 strategy to allow lawyers to work more efficiently from
+                home.
+              </p>
+            </Fade>
+          </div>
         </Grid>
       </Grid>
     </div>
